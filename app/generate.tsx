cb701a24f5093b6fa673d11d90cd2a0b296ca684
barlos <9cguizar2004@gmail.com>
2025-09-16 13:11:40 -0700

    Create generate.tsx

---
+++ app/generate.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+import { Text, View, TouchableOpacity, FlatList, Image} from "react-native";
+import * as AuthSession from "expo-auth-session";
+import { router } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+export default function Generate(){
+    const 
+
+    return(
+        <Text>Balls</Text>
+    );
+}
